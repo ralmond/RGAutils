c32051ef415568f6385ce47ae474b38e0220b473
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2016-11-01 00:34:40 +0000

    Data set generated.

--- RGAutils/trunk/inst/extraTests/dataGen.R
+++ RGAutils/trunk/inst/extraTests/dataGen.R
@@ -28,3 +28,58 @@ twoskill.data1 <- data.frame(theta,obs1)
 save(twoskill.data0,twoskill.data1,twoskill.B,twoskill.b0,twoskill.Q,
      twoskill.resid.sd,
      file="RGAutils/data/twoskill.RData")
+
+
+
+####
+## BQMiss Model
+
+bqm.n <- 1000
+
+bqm.Sigma <- diag(3)
+bqm.Sigma[1,2] <- bqm.Sigma[2,1] <- .6
+bqm.Sigma[1,3] <- bqm.Sigma[3,1] <- -.5
+bqm.Sigma[2,3] <- bqm.Sigma[3,2] <- -.4
+
+bqm.Theta <- rmvnorm(bqm.n,sigma=bqm.Sigma)
+colnames(bqm.Theta) <- paste("Skill",1:ncol(bqm.Theta),sep="")
+
+bqm.B <- matrix(c(1/3,1/3,1/3,
+                  1/2,1/4,1/4,
+                  .8,.2,.2,
+                  .2,.8,.2,
+                  .2,.2,.8,
+                  2/3,0,1/3,
+                  1/2,1/2,0,
+                  1,0,0,
+                  .8,0,0,
+                  .4,.6,0
+                  ),10,3,byrow=TRUE,
+                dimnames=list(obs=paste("Test",1:10,sep=""),
+                              skill=colnames(bqm.Theta))
+                )
+bqm.Q <- bqm.B > 0
+
+bqm.B0 <- c(-1,-1,1,1,1,-1,0,0,0,0)
+
+bqm.obs0 <- sweep(bqm.Theta%*%t(bqm.B),2,bqm.B0,"+")
+bqm.resid.sd <- diag(rep(.4,10))
+bqm.obs1 <- bqm.obs0 + rmvnorm(bqm.n,sigma=bqm.resid.sd)
+
+## Randomly make some data missing.
+bqm.obs2 <- bqm.obs1
+bqm.m <- matrix(FALSE,bqm.n,10)
+for (k in 1:5) {
+  bqm.m[sample.int(bqm.n,round(.1*k*bqm.n)),k] <- TRUE
+  is.na(bqm.obs2[bqm.m[,k],k]) <- TRUE
+}
+
+bqm.data0 <- data.frame(bqm.Theta,bqm.obs0)
+bqm.data1 <- data.frame(bqm.Theta,bqm.obs1)
+bqm.data2 <- data.frame(bqm.Theta,bqm.obs2)
+
+save(bqm.data0,bqm.data1,bqm.data2,
+     bqm.Theta,bqm.obs0,bqm.obs1,bqm.obs2,
+     bqm.B,bqm.B0,bqm.Q,
+     bqm.resid.sd,bqm.Sigma,
+     file="~/Projects/RGAutils/data/bqm.RData")
